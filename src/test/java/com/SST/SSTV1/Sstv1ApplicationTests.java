0406778ec5e57c1a6d61184b05b8785ce476eeba
CodingSimian <viktor.bernas.ostrand@edu.edugrade.se>
2023-09-25 13:08:49 +0200

    Initial commit

---
+++ src/test/java/com/SST/SSTV1/Sstv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SST.SSTV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sstv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
